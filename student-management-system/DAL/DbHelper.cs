7385e3dd9704909c61523764a2738385521eaf04
yohaseelan <yohaseelan@outlook.com>
2025-06-24 11:02:54 +0530

    Add DbHelper class for MySQL database operations
    Introduced DbHelper in the DAL to centralize MySQL database access, including methods for connection management, data retrieval, and command execution. Updated project references to include MySql.Data and System.Configuration, and added the new DAL/DbHelper.cs file to the project.

---
+++ student-management-system/DAL/DbHelper.cs
@@ -0,0 +1,68 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student_management_system.DAL
+{
+    internal class DbHelper
+    {
+        private static readonly string connectionString = ConfigurationManager.ConnectionStrings["MySqlStudentDB"].ConnectionString;
+
+
+        public static MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+
+
+        public static DataTable GetData(string query, params MySqlParameter[] parameters)
+        {
+            using (var connection = GetConnection())
+            using (var command = new MySqlCommand(query, connection))
+            {
+                if (parameters != null)
+                    command.Parameters.AddRange(parameters);
+
+                var adapter = new MySqlDataAdapter(command);
+                var dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                return dataTable;
+            }
+        }
+
+
+        public static int ExecuteNonQuery(string query, params MySqlParameter[] parameters)
+        {
+            using (var connection = GetConnection())
+            using (var command = new MySqlCommand(query, connection))
+            {
+                connection.Open();
+                if (parameters != null)
+                    command.Parameters.AddRange(parameters);
+
+                return command.ExecuteNonQuery();
+            }
+        }
+
+
+        public static object ExecuteScalar(string query, params MySqlParameter[] parameters)
+        {
+            using (var connection = GetConnection())
+            using (var command = new MySqlCommand(query, connection))
+            {
+                connection.Open();
+                if (parameters != null)
+                    command.Parameters.AddRange(parameters);
+
+                return command.ExecuteScalar();
+            }
+        }
+
+    }
+}
