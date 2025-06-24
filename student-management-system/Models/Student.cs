50858563008280772dd597ecc92ae790ee8ce454
yohaseelan <yohaseelan@outlook.com>
2025-06-24 11:44:35 +0530

    Update Student model and assembly name
    Changed EnrollmentDate and CreatedAt in Student model from string to DateTime for better type safety. Updated the assembly name in the project file to 'StudentManagementSystem' for consistency.

--- student-management-system/Models/Student.cs
+++ student-management-system/Models/Student.cs
@@ -13,11 +13,11 @@ namespace student_management_system.Models
         public string LastName { get; set; }
         public DateTime DateOfBirth { get; set; }
 
-      public string  Gender { get; set; }
+        public string  Gender { get; set; }
         public string Address {get;set;}
         public string Email {get;set;}
         public string Phone { get; set; }
-        public string EnrollmentDate { get; set; }
-        public string CreatedAt { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public DateTime CreatedAt { get; set; }
     }
 }
